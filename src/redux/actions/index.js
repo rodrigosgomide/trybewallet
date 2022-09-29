7c470b58f3157c7d7b21b63233205f0105787386
rodrigosgomide <rodrigogomide.def@gmail.com>
2022-09-29 17:29:35 -0300

    Redux - config

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,11 @@
 // Coloque aqui suas actions
+const SUCCESS_LOGIN = 'SUCCESS_LOGIN';
+
+const addEmail = (payload) => ({
+  type: SUCCESS_LOGIN,
+  payload,
+});
+
+export default {
+  addEmail,
+};
--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,1 +1,18 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+
+const INITIAL_STATE = {
+  user: {
+    email: '', // string que armazena o email da pessoa usuária
+  },
+};
+
+function login(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case SUCCESS_LOGIN:
+    return { ...state, email: action.payload.email };
+  default:
+    return state;
+  }
+}
+
+export default login;
